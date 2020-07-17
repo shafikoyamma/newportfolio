98e9eefa6e1b08cd65a2a83df6b1654975e641e6
shafi koyamma <shafikoyamma@gmail.com>
2020-07-17 19:10:01 +0300

    spelling editing and added icons

--- scriptP2.js
+++ scriptP2.js
@@ -1,9 +1,11 @@
 var btnhome = document.querySelector("#btnhome");
 var btnExperience=document.querySelector("#btnExperience");
 var btngraphic = document.querySelector("#btnDesign");
+var btnVideo = document.querySelector("#btnVideo")
 // var contactbtn = document.querySelector("#contactbtn");
 
 var designDisplay = document.querySelector("#designDisplay");
+var videoDisplay = document.querySelector("#videoDisplay")
 // var experience = document.getElementById("experience");
 // var contactDisplay = document.querySelector("#contactDisplay");
 var imgnum = 0;
@@ -26,6 +28,7 @@ function imgsloop(){
 
 function display(){
     designDisplay.classList.add("display");
+    videoDisplay.classList.add("display");
     // experience.classList.add("display");
     // contactDisplay.classList.add("display");
 }
@@ -34,23 +37,26 @@ function active(){
     btnhome.classList.remove("active");
     // contactbtn.classList.remove("active");
     btngraphic.classList.remove("active");
+    btnVideo.classList.remove("active")
     // btnExperience.classList.remove("active");
 }
 
 function onloadfunction(){
-    imgsloop();
+
     // imgnum = 1;
 
  
     active();
-    // display()
+    display();
+    designDisplay.classList.remove("display");
     btngraphic.classList.add("active");
+    imgsloop();
     
     
 }
 // experience.style.display="none";
 
-btnhome.addEventListener("click", onloadfunction);
+// btnhome.addEventListener("click", onloadfunction);
 
 btngraphic.addEventListener("click", function(){
     active();
@@ -59,19 +65,26 @@ btngraphic.addEventListener("click", function(){
     designDisplay.classList.remove("display");
 });
 
+btnVideo.addEventListener("click", function(){
+    active();
+    display();
+    this.classList.add("active");
+    videoDisplay.classList.remove("display");
+})
+
 
 
 
 document.querySelector("#spnvideo").addEventListener("click", function(){
-    this.classList.toggle("display");
-    document.querySelector("#spngraphic").classList.toggle("display");
+    document.querySelector("#spnvideo").classList.add("display");
+    document.querySelector("#spngraphic").classList.remove("display");
     document.querySelector("#graphicWorks").classList.toggle("display");
     document.querySelector("#videoEdited").classList.toggle("display");
 
 
 });
 document.querySelector("#spngraphic").addEventListener("click", function(){
-    this.classList.toggle("display");
+    document.querySelector("#spngraphic").classList.toggle("display");
     document.querySelector("#spnvideo").classList.toggle("display");
     document.querySelector("#videoEdited").classList.toggle("display");
     document.querySelector("#graphicWorks").classList.toggle("display");
